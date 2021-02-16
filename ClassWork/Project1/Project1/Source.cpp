f4a062854158c87c4ed84c0419ad56fe673e36f4
Nazar Koval <nazarkovall75@gmail.com>
2021-02-16 20:22:29 +0200

    First app

---
+++ ClassWork/Project1/Project1/Source.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+
+/*
+long
+comment
+*/
+
+#include <iostream>
+
+int main()   //декларація функції
+{
+	float a = 3.14f;
+	std::cout << "Hello world" << '\t' << 45 << std::endl << a;
+	return 0;
+}
