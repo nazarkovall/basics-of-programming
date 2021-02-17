5c0f6e002447f13ec99cb0793aff66eaabdc640b
Nazar Koval <nazarkovall75@gmail.com>
2021-02-17 21:09:27 +0200

    Second app

--- ClassWork/Project1/Project1/Source.cpp
+++ ClassWork/Project1/Project1/Source.cpp
@@ -7,7 +7,7 @@ comment
 
 #include <iostream>
 
-int main()   //декларація функції
+int main()
 {
 	float a = 3.14f;
 	std::cout << "Hello world" << '\t' << 45 << std::endl << a;
---
+++ HomeWork/C++ as a calculator/C++ as a calculator/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	/*
+	// Find area of the triangle
+	int a, b, c, p, S;
+
+	cout << "a= ";
+	cin >> a;
+	cout << "b= ";
+	cin >> b;
+	cout << "c= ";
+	cin >> c;
+
+	p = (a + b + c) / 2;
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+
+	cout << "Area is = " << S << endl;
+	*/
+
+
+	system("pause");
+	return 0;
+}
